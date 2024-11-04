f76da8ef8c6f4e69c6b45c64703b451a4c24d51c
AlanaSR <svetlanka.shkola@gmail.com>
2024-11-04 11:49:01 +0500

    initialization

---
+++ src/main/java/com/hwbasket/homeworkbasket/HomeworkbasketApplication.java
@@ -0,0 +1,13 @@
+package com.hwbasket.homeworkbasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkbasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkbasketApplication.class, args);
+	}
+
+}
